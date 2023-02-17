b12a070af6bfa11a48376719426f9332ced7f0af
Eric Hardy <erichardy011@gmail.com>
2023-02-17 03:11:22 -0600

    changed header logo into a link to homepage

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,11 +5,11 @@ import NewPost from "../img/New-post.svg"
 export default function Header() {
     return (
         <header>
-            <img src={InstagramLogo} alt="Instagram Logo" className="header-logo" />
+            <a href="/"><img src={InstagramLogo} alt="Instagram Logo" className="header-logo" /></a>
             <nav>
                 <ul className="nav-list">
                     <li>
-                        <a href="/#" id="header-modal"><img src={NewPost} alt=""/></a>
+                        <a href="/" id="header-modal"><img src={NewPost} alt=""/></a>
                     </li>
                     <li>
                         <a href="/notifcations"><img src={Notifications} alt="Notifications Button" /></a>
